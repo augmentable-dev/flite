ef7d1f245fce58c4596470fcd5fb078150b98ce4
vialeon <dnewberry21@amherst.edu>
2021-05-03 17:23:20 -0400

    add a few more headers

--- internal/http/helpers_test.go
+++ internal/http/helpers_test.go
@@ -3,8 +3,8 @@ package http
 import "testing"
 
 func TestHeaderParser(t *testing.T) {
-	header := "Content-Type: application/json |Accept: application/json"
-	expected := [][]string{{"Content-Type", "application/json"}, {"Accept", "application/json"}}
+	header := "Content-Type: application/json |Accept: application/json|accept-encoding: application/gzip|x-api-key: 09187304915uqiyoewue90832174109732y6985132"
+	expected := [][]string{{"Content-Type", "application/json"}, {"Accept", "application/json"}, {"accept-encoding", "application/gzip"}, {"x-api-key", "09187304915uqiyoewue90832174109732y6985132"}}
 	parsed := ParseHeaders(header)
 	for i, _ := range expected {
 		if expected[i][0] != parsed[i][0] || expected[i][1] != parsed[i][1] {
