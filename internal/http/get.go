e13fda1b6497196d869ffeccdc09bbe312bbcb9c
vialeon <dnewberry21@amherst.edu>
2021-05-03 15:14:45 -0400

    splitting get into functions

--- internal/http/get.go
+++ internal/http/get.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"go.riyazali.net/sqlite"
 )
@@ -17,9 +18,9 @@ func (m *get) Deterministic() bool { return false }
 func (m *get) Apply(ctx *sqlite.Context, values ...sqlite.Value) {
 	var (
 		request  string
+		headers  [][]string
 		err      error
-		contents []byte
-		response *http.Response
+		contents string
 	)
 
 	if len(values) > 0 {
@@ -28,17 +29,39 @@ func (m *get) Apply(ctx *sqlite.Context, values ...sqlite.Value) {
 		err := errors.New("input a single url as the argument to http get")
 		ctx.ResultError(err)
 	}
-
-	response, err = http.Get(request)
-	if err != nil {
-		ctx.ResultError(err)
-	}
-	contents, err = ioutil.ReadAll(response.Body)
+	contents, err = httpGet(request, headers)
 	if err != nil {
 		ctx.ResultError(err)
 	}
 
-	ctx.ResultText(string(contents))
+	ctx.ResultText(contents)
+}
+func parseHeaders(headers string) [][]string {
+	headerList := strings.Split(headers, "|")
+	var kvHeaders [][]string
+	for _, s := range headerList {
+		kvHeaders = append(kvHeaders, strings.Split(s, ":"))
+	}
+	return kvHeaders
+}
+func httpGet(requestUrl string, headers [][]string) (string, error) {
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", requestUrl, nil)
+	if err != nil {
+		return "", err
+	}
+	for _, header := range headers {
+		request.Header.Add(header[0], header[1])
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(contents), nil
 }
 
 // NewHTTPGet returns a sqlite function for reading the contents of a file
